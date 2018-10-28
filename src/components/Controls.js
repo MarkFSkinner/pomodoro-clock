332125dee269fe23b5ebdd925eb407a2ec64e6d4
Mark Skinner <markskinner850@gmail.com>
2018-10-28 10:39:48 -0400

    Add formating and data to Controls component

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,8 +1,43 @@
 import React from 'react';
 
-const Controls = () => (
-  <div>
-    <h1>Controls</h1>
+const Controls = props => (
+  <div className="row" id="controls">
+    <div className="offset-2 col-4">
+      <div className="row">
+        <div className="col-12">
+          <h5>BREAK LENGTH</h5>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-4 offset-sm-1 col-sm-3">
+          <h3 id="break-decrease" className="clickable">-</h3>
+        </div>
+        <div className="col-4">
+          <h3 id="break-time">{props.breakTime}</h3>
+        </div>
+        <div className="col-4 col-sm-3">
+          <h3 id="break-increase" className="clickable">+</h3>
+        </div>
+      </div>
+    </div>
+    <div className="col-4">
+      <div className="row">
+        <div className="col-12">
+          <h5>SESSION LENGTH</h5>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-4 offset-sm-1 col-sm-3">
+          <h3 id="session-decrease" className="clickable">-</h3>
+        </div>
+        <div className="col-4">
+          <h3 id="session-time">{props.sessionTime}</h3>
+        </div>
+        <div className="col-4 col-sm-3">
+          <h3 id="session-increase" className="clickable">+</h3>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
